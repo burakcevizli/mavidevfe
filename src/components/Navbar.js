515392dc962b581ec60770699cbb53ec2cf15193
KrieqqeR <burakcevizli@gmail.com>
2024-08-03 21:22:46 +0300

    overflow y hidden eklendi.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="bg-blue-500 text-white">
+    <div className="bg-blue-500 over text-white">
       <nav className="container mx-auto p-4 flex justify-between items-center">
-        <div className="text-2xl font-bold">MyApp</div>
+        <div className="text-2xl font-bold">Mavidev</div>
         <ul className="flex space-x-4">
-          <li className="hover:text-gray-300 transition duration-300"><a href="#home">Sehir Ekle</a></li>
-          <li className="hover:text-gray-300 transition duration-300"><a href="#about">Sehir Listele</a></li>
+          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            Sehir-İlce Ekle
+          </button>
+          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+            Sehir-İlce Listele
+          </button>
         </ul>
       </nav>
     </div>
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,21 +1,20 @@
 import React, { useState } from 'react';
 
 const WelcomePage = () => {
-    const [goster, setGoster] = useState(false);
 
     const onClickHandler = () => {
-        setGoster(true);
+        alert("Lütfen yukarıdaki navbardan bir seçenek seçiniz.")
     }
 
     return (
-        <div className="flex items-center justify-center h-screen bg-gradient-to-r from-blue-800 to-indigo-800">
+        <div className="flex items-center justify-center h-screen  bg-gradient-to-r from-blue-800 to-indigo-800">
             <div className="text-center text-white p-10 bg-opacity-75 bg-black rounded-lg shadow-lg">
                 <h1 className="text-6xl font-bold my-4 animate-bounce">Mavidev Sunum Projeme Hoşgeldiniz...</h1>
                 <div className='flex flex-col gap-4'>
                     <p className="text-xl">Sehir ekleme ve listeleme ekranlarını görüntüleyeceksiniz.</p>
                     <p className="text-xl">Görüntülemek istediğiniz seçeneği butona bastıktan sonra seçebilirsiniz.</p>
                 </div>
-                <button className="mt-8 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold">
+                <button onClick={() => onClickHandler()} className="mt-8 px-6 py-3 bg-white text-blue-500 rounded-full font-semibold">
                     Hadi Başlayalım.
                 </button>
             </div>
