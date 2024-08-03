c272b8589d085bd653f23ba913240f32cb5f7d3f
KrieqqeR <burakcevizli@gmail.com>
2024-08-03 22:02:36 +0300

    Mavidev yazısına cursor-pointer eklendi.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div className="bg-blue-500 over text-white">
             <nav className="container mx-auto p-4 flex justify-between items-center">
-                <div className="text-2xl font-bold">Mavidev</div>
+                <h1 className="text-2xl font-bold cursor-pointer" onClick={() => navigate('/')}>Mavidev</h1>
                 <ul className="flex space-x-4">
                     <button onClick={() => navigate('/')} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                         Anasayfa
