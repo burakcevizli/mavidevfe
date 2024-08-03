c75380187e0a5b442c90316bc71bfc9a78fb04f8
KrieqqeR <burakcevizli@gmail.com>
2024-08-03 22:00:48 +0300

    button ve navbara yönlendirme eklendi.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  return (
-    <div className="bg-blue-500 over text-white">
-      <nav className="container mx-auto p-4 flex justify-between items-center">
-        <div className="text-2xl font-bold">Mavidev</div>
-        <ul className="flex space-x-4">
-          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-            Sehir-İlce Ekle
-          </button>
-          <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
-            Sehir-İlce Listele
-          </button>
-        </ul>
-      </nav>
-    </div>
-  );
+    const navigate = useNavigate();
+
+    return (
+        <div className="bg-blue-500 over text-white">
+            <nav className="container mx-auto p-4 flex justify-between items-center">
+                <div className="text-2xl font-bold">Mavidev</div>
+                <ul className="flex space-x-4">
+                    <button onClick={() => navigate('/')} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        Anasayfa
+                    </button>
+                    <button onClick={() => navigate('sehir-ekle')} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        Sehir-İlce Ekle
+                    </button>
+                    <button onClick={() => navigate('sehir-listele')} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                        Sehir-İlce Listele
+                    </button>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Navbar;
--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const WelcomePage = () => {
 
+    const navigate = useNavigate();
+
     const onClickHandler = () => {
-        
+        navigate('/sehir-ekle')
     }
 
     return (
