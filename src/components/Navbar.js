c990da7e06d5a66b849dc61986eefec61d8ad2cc
KrieqqeR <burakcevizli@gmail.com>
2024-08-08 17:28:54 +0300

    Navbar Onclick handler eklendi.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,14 @@ import { useNavigate } from 'react-router-dom';
 const Navbar = () => {
     const navigate = useNavigate();
 
+    const onClickHandler = () =>{
+        navigate('/')
+    }
+
     return (
         <div className="bg-blue-500 text-white">
             <nav className="container mx-auto p-4 flex justify-between items-center">
-                <h1 className="text-2xl font-bold cursor-pointer" onClick={() => navigate('/')}>Mavidev</h1>
+                <h1 className="text-2xl font-bold cursor-pointer" onClick={()=>onClickHandler()}>Mavidev</h1>
                 <ul className="flex space-x-4">
                     <button onClick={() => navigate('/')} type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
                         Anasayfa
