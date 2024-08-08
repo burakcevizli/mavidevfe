16fe3e4d49e66035ef1b2dd1e6c869d24de79678
KrieqqeR <burakcevizli@gmail.com>
2024-08-08 16:43:44 +0300

    Ortalandı.

--- src/components/SehirListele.js
+++ src/components/SehirListele.js
@@ -28,27 +28,27 @@ const SehirListele = () => {
   return (
     <div className='min-h-screen bg-slate-700'>
       <div className='w-[80%] mx-auto pt-32'>
-        <div className="relative overflow-x-auto">
           <table className="w-full text-sm text-left">
             <thead className="bg-gray-900 text-white">
               <tr>
-                <th scope="col" className="px-6 py-3 underline border-b border-gray-700">ID</th>
-                <th scope="col" className="px-6 py-3 underline border-b border-gray-700">Şehir</th>
-                <th scope="col" className="px-6 py-3 underline border-b border-gray-700">İlçe</th>
+                <th scope="col" className="px-6 py-3 underline text-center border-b border-gray-700">ID</th>
+                <th scope="col" className="px-6 py-3 underline text-center border-b border-gray-700">Şehir</th>
+                <th scope="col" className="px-6 py-3 underline border-b text-center border-gray-700">İlçe</th>
+                <th scope="col" className="px-6 py-3 underline border-b text-center border-gray-700">İşlem</th>
               </tr>
             </thead>
             <tbody>
               {sehirIlceler.length > 0 ? (
                 sehirIlceler.map((item) => (
                   <tr key={item.id} className="hover:bg-white bg-slate-400 transition-colors">
-                    <td className="px-6 py-4">{item.id}</td>
-                    <td className="px-6 py-4">{item.sehir}</td>
-                    <td className="px-6 py-4">{item.ilce}</td>
-                    <td>
+                    <td className="px-6 text-center py-4">{item.id}</td>
+                    <td className="px-6 text-center py-4">{item.sehir}</td>
+                    <td className="px-6 text-center py-4">{item.ilce}</td>
+                    <td className='text-center'>
                       <button
                         type="submit"
                         onClick={()=>buttonHandler(item.id)}
-                        className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full py-2.5 text-center"
+                        className="text-white bg-blue-600 hover:bg-blue-700 font-medium rounded-lg text-sm px-8 py-2.5 text-center"
                       >
                         Sil
                       </button></td>
@@ -61,7 +61,6 @@ const SehirListele = () => {
               )}
             </tbody>
           </table>
-        </div>
       </div>
     </div>
   );
